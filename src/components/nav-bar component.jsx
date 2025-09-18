6c2e16a0958b6f5289ff0fb7159cf64d19a4ee2a
AGpeter2018 <adenijipeter2018@gmail.com>
2025-09-18 07:38:26 +0100

    added an underline image to the nav bar

--- src/components/nav-bar component.jsx
+++ src/components/nav-bar component.jsx
@@ -1,19 +1,36 @@
-import React from "react";
+import { React, useState } from "react";
 
 import logo from "../assets/logo.svg";
+import underline from "../assets/nav_underline.svg";
 
 import "./nav-bar.style.css";
 
 const NavBar = () => {
+  const [menu, setMenu] = useState("home");
   return (
     <div className="nav-bar">
       <img src={logo} alt="" />
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>About Me</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+        <li>
+          <p onClick={() => setMenu("home")}>Home</p>
+          {menu === "home" ? <img src={underline} alt="" /> : ""}
+        </li>
+        <li>
+          <p onClick={() => setMenu("contact")}>About Me</p>
+          {menu === "about " ? <img src={underline} alt="" /> : ""}
+        </li>
+        <li>
+          <p onClick={() => setMenu("services")}>Services</p>
+          {menu === "services" ? <img src={underline} alt="" /> : ""}
+        </li>
+        <li>
+          <p onClick={() => setMenu("portfolio")}>Portfolio</p>
+          {menu === "portfolio" ? <img src={underline} alt="" /> : ""}
+        </li>
+        <li>
+          <p onClick={() => setMenu("contact")}>Contact</p>
+          {menu === "contact" ? <img src={underline} alt="" /> : ""}
+        </li>
       </ul>
       <div className="nav-connect">Connect with me</div>
     </div>
